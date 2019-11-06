c32e0094cafa7243b84512a0af51fb004c263fde
qg5250wa <qg5250wa@W3110-S1664710.century.edu>
2019-11-06 13:55:50 -0600

    Add RankingBoardGUI

--- 1082-Employee-Rewards-System/src/edu/century/rewards_system/Driver.java
+++ 1082-Employee-Rewards-System/src/edu/century/rewards_system/Driver.java
@@ -2,8 +2,6 @@ package edu.century.rewards_system;
 
 public class Driver {
 	public static void main(String[] args) {
-		Employee emp = new Employee(10);
-		for (int i = 0; i < 10; i++)
-			System.out.println((new Employee(10)).getName());
+		new RankingBoardGUI("Employee Ranking System");
 	}
 }
---
+++ 1082-Employee-Rewards-System/src/edu/century/rewards_system/RankingBoardGUI.java
@@ -0,0 +1,18 @@
+package edu.century.rewards_system;
+
+import javax.swing.*;
+/*
+ * GUI Elements to add:
+ * Toolbar [top]
+ * List [center, possible GridLayout]
+ */
+public class RankingBoardGUI extends JFrame {
+
+	public RankingBoardGUI(String winTitle) {
+		super(winTitle);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 450);
+		setVisible(true);
+	}
+	
+}
